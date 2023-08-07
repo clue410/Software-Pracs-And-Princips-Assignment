2c4c85c69130c9864b658a2394bdfc170e9ee728
Mars :) <marilena.sayfullina@gmail.com>
2023-08-07 11:51:25 +1000

    :0

---
+++ src/main/java/repository/ProductRepository.java
@@ -0,0 +1,5 @@
+package repository;
+import model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
