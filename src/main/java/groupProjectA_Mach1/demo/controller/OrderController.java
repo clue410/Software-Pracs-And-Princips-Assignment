467ad8182c0a0183497bf4328d3610b4fc7df926
Mars :) <marilena.sayfullina@gmail.com>
2023-08-07 21:53:32 +1000

    figured out workings, started POST,GET,PUT mapping on Product moved packages to correct location (inside main package)

--- src/main/java/groupProjectA_Mach1/demo/controller/OrderController.java
+++ src/main/java/groupProjectA_Mach1/demo/controller/OrderController.java
@@ -1,14 +1,7 @@
 package groupProjectA_Mach1.demo.controller;
-import model.Order;
-import repository.OrderRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
-
 public class OrderController {
 
 //    private final BookRepository bookRepository;
--- src/main/java/groupProjectA_Mach1/demo/controller/ProductController.java
+++ src/main/java/groupProjectA_Mach1/demo/controller/ProductController.java
@@ -1,12 +1,49 @@
 package groupProjectA_Mach1.demo.controller;
-import model.Product;
-import repository.ProductRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+
+import groupProjectA_Mach1.demo.model.Product;
+import groupProjectA_Mach1.demo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/product")
+    void createProduct() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @PutMapping("/product/{id}")
+    void updateProduct(@PathVariable Long id) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @GetMapping("/product/{id}")
+    Product getProduct(@PathVariable Long id) {
+        return productService.getById(id);
+    }
+
+    @PostMapping("/product/{id}/detail")
+    void createProductDetail(@PathVariable Long id) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @PutMapping("/product/{id}/detail")
+    void updateProductDetail(@PathVariable Long id) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @GetMapping("/product/{id}/detail")
+    void getProductDetail(@PathVariable Long id) {
+        throw new RuntimeException("Not implemented");
+    }
+
+
 }
