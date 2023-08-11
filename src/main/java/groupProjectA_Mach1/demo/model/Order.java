6c7a15802bdf91435c2546225e677b3768c6facf
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-08-11 14:00:40 +1000

    Update Order.java

--- src/main/java/groupProjectA_Mach1/demo/model/Order.java
+++ src/main/java/groupProjectA_Mach1/demo/model/Order.java
@@ -1,18 +1,73 @@
 package groupProjectA_Mach1.demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
 
-@Entity
 public class Order {
-
     @Id
     @GeneratedValue
-    private long id;
-    private String supplier;
-    private String product;
-    private int quantity;
+    public long id;
+    public String supplier;
+    @ManyToMany(cascade=CascadeType.PERSIST)
+    @JoinColumn(name = "product_id")
+    @JsonIgnore
+    public Product product;
+    public int quantity;
 
+    public Order(){ }
 
+    public Order(long id,
+                 String supplier,
+                 Product product,
+                 int quantity) {
+        this.id = id;
+        this.supplier = supplier;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Order(String supplier,
+                 Product product,
+                 int quantity) {
+        this.supplier = supplier;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "supplier='" + supplier + '\'' +
+                ", product=" + product +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
