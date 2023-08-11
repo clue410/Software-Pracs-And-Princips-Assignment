f106f71b5939328a1c3c7a254e38e4933e2f2461
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-08-11 14:01:59 +1000

    Update OrderController.java

--- src/main/java/groupProjectA_Mach1/demo/controller/OrderController.java
+++ src/main/java/groupProjectA_Mach1/demo/controller/OrderController.java
@@ -1,22 +1,38 @@
 package groupProjectA_Mach1.demo.controller;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Order;
+import com.example.demo.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OrderController {
 
-//    private final BookRepository bookRepository;
-//
-//    BookController(BookRepository bookRepository) {
-//        this.bookRepository= bookRepository;
-//    }
-//
-//    @GetMapping("/books")
-//    List<Book> all() {
-//        return bookRepository.findAll();
-//    }
-//
-//    @PostMapping("/books")
-//    Book newBook(@RequestBody Book newBook) {
-//        return bookRepository.save(newBook);
-//    }
+    private final OrderService orderService;
+
+    //constructor
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    //create new order
+    @PostMapping("/order")
+    public ResponseEntity createNewOrder(@RequestBody Order order) {
+        //return orderService.createNewOrder(order.supplier, order.product, order.quantity);
+        orderService.createNewOrder(order.supplier, order.product, order.quantity);
+        return ResponseEntity.ok("Order Created!");
+    }
+
+    //update order
+    @PutMapping("/order/{id}")
+    public ResponseEntity updateOrder(@PathVariable Long id, @RequestBody Order currentOrder) {
+        Order order = null;
+        if (orderService.findOrderById(id) == null) {
+            return ResponseEntity.ok("Order Not Found!");
+        } 
+        else {
+            orderService.updateOrder(id, order.supplier, order.product, order.quantity);
+            return ResponseEntity.ok("Order Updated!");
+        }
+    }
 }
