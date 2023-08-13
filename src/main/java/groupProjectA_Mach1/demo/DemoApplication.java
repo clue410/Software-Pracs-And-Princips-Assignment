a656b6a6fbccf3e43097ca9906d2dd9fecf77773
Rijul74585 <rss505@uowmail.edu.au>
2023-08-13 13:17:34 +1000

    customer

--- src/main/java/groupProjectA_Mach1/demo/DemoApplication.java
+++ src/main/java/groupProjectA_Mach1/demo/DemoApplication.java
@@ -3,11 +3,21 @@ package groupProjectA_Mach1.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import groupProjectA_Mach1.demo.model.Customer;
+
 @SpringBootApplication
 public class DemoApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("hanji kya haal");
+
+		//Customer c= context.getBean(Customer.class);
+
+	
+
 	}
 
 }
--- src/main/java/groupProjectA_Mach1/demo/model/Customer.java
+++ src/main/java/groupProjectA_Mach1/demo/model/Customer.java
@@ -4,7 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.springframework.stereotype.Component;
+
 @Entity
+@Component
 public class Customer {
 
     @Id
@@ -13,4 +16,70 @@ public class Customer {
     private String companyName;
     private String address;
     private String country;
+    private Contact contact;
+
+    public Customer(){
+
+    }
+
+    public Customer(String companyName, String address, String country){
+        this.id=id;
+        this.address=address;
+        this.companyName=companyName;
+        this.country=country;
+    }
+
+    public Customer(String companyName, String address, String country, Contact contact){
+        this.id=id;
+        this.address=address;
+        this.companyName=companyName;
+        this.country=country;
+        this.contact=contact;
+    }
+
+    public Contact getContact() {
+        return this.contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return this.companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+ 
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void print(){
+        System.out.println("gijntjgntrng");
+    }
 }
