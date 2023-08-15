b1fffa7bf5af566dc2df71c203e14fcebec40e61
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-08-15 22:15:07 +1000

    Update ProductService.java

--- src/main/java/groupProjectA_Mach1/demo/services/ProductService.java
+++ src/main/java/groupProjectA_Mach1/demo/services/ProductService.java
@@ -1,10 +1,10 @@
 package groupProjectA_Mach1.demo.services;
 
 import groupProjectA_Mach1.demo.model.Product;
+import groupProjectA_Mach1.demo.model.ProductDetail;
 import groupProjectA_Mach1.demo.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,15 +16,40 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    List<Product> findProductsByBlah() {
-        return productRepository.findAll();//todo real stuff
-    }
-
     public Product getById(Long id) {
         Optional<Product> product = productRepository.findById(id);
         if (product.isPresent()) {
             return product.get();
         }
         return null;
+    }
+
+    public long createNewProduct(String category, String name, double price) {
+        Product product = new Product(category, name, price);
+        Product savedProduct = productRepository.save(product);
+        return savedProduct.getId();
+    }
+
+    public void updateProduct(Long id, String productCategory, String name, double price) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        Product product = productOptional.get();
+        product.setProductCategory(productCategory);
+        product.setName(name);
+        product.setPrice(price);
+        productRepository.save(product);
+    }
+
+    public void updateProductDetails(Long id, String description, String comment) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        Product product = productOptional.get();
+        ProductDetail productDetails = productOptional.get().getProductDetails();
+        productDetails.setId(id);
+        productDetails.setDescription(description);
+        productDetails.setComment(comment);
+        productRepository.save(product);
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
     }
 }
