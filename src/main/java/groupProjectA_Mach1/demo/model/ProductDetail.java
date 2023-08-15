2032160dc35cd4ca023533fcfd38376d055ef222
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-08-15 22:12:17 +1000

    Update ProductDetail.java

--- src/main/java/groupProjectA_Mach1/demo/model/ProductDetail.java
+++ src/main/java/groupProjectA_Mach1/demo/model/ProductDetail.java
@@ -1,15 +1,45 @@
 package groupProjectA_Mach1.demo.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class ProductDetail {
     @Id
-    @GeneratedValue
     private long id;
-
     private String description;
     private String comment;
+
+    public ProductDetail() {
+    }
+
+    public ProductDetail(long id, String description, String comment) {
+        this.id = id;
+        this.description = description;
+        this.comment = comment;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
