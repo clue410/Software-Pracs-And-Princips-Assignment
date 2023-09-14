427214cb0110d06dcfee1da9d621c5b8922015dd
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-09-15 09:07:01 +1000

    Delete sales-service directory

--- sales-service/src/main/java/groupProjectB/demo/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-
-package groupProjectB.demo.repository;
-
-import groupProjectB.demo.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
