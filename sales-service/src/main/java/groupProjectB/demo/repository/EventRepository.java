60712ef79bf84d1f266e6a797d3f41875f26737b
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-09-15 12:01:27 +1000

    Delete sales-service directory

--- sales-service/src/main/java/groupProjectB/demo/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package groupProjectB.demo.repository;
-
-import groupProjectB.demo.model.event.OrderEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<OrderEvent, Long> {
-
-}
