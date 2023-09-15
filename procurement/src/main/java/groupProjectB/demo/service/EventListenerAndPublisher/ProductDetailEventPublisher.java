7f8f896a954cca2c1d03687b7afc46bbc370115d
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-09-15 14:14:29 +1000

    Update ProductDetailEventPublisher.java

--- procurement/src/main/java/groupProjectB/demo/service/EventListenerAndPublisher/ProductDetailEventPublisher.java
+++ procurement/src/main/java/groupProjectB/demo/service/EventListenerAndPublisher/ProductDetailEventPublisher.java
@@ -1,23 +1,21 @@
-//package groupProjectB.demo.service.EventListenerAndPublisher;
-////
-////public class ProductDetailEventPublisher {
-////}
-//import groupProjectB.demo.model.productEvent.ProductDetailEvent;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Lazy;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ProductDetailEventPublisher {
-//    @Autowired
-//    @Lazy
-//    private ProductDetailEventPublisher productDetailEventPublisher;
-//
-//    public void publishProductDetailEvent(final String message){
-//        System.out.println("Product Detail Event Published!");
-//        ProductDetailEvent productDetailEvent = new ProductDetailEvent(this, message);
-//        productDetailEventPublisher.publishProductDetailEvent("Message Message woohoo");
-//        System.out.println("RAHHH Product Detail Event Publisher");
-//    }
-//
-//}
+package groupProjectB.demo.service.EventListenerAndPublisher;
+
+import groupProjectB.demo.model.productEvent.ProductDetailEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductDetailEventPublisher {
+   @Autowired
+   @Lazy
+   private ProductDetailEventPublisher productDetailEventPublisher;
+
+   public void publishProductDetailEvent(final String message){
+       System.out.println("Product Detail Event Published!");
+       ProductDetailEvent productDetailEvent = new ProductDetailEvent(this, message);
+       productDetailEventPublisher.publishProductDetailEvent("Message Message woohoo");
+       System.out.println("RAHHH Product Detail Event Publisher");
+   }
+
+}
