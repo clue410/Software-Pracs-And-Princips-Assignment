b8592c8f533ab82fba90b8f625543a4a55e26d81
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-10-06 07:58:33 +1100

    Delete customerAccount directory

--- customerAccount/src/main/java/csci318/demo/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package csci318.demo.repository;
-
-import csci318.demo.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
