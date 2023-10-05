21196c4431c4499c0a3ee322b3a4a93df7e45132
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-10-05 22:09:02 +1100

    Delete sales directory

--- sales/src/main/java/csci318/demo/repository/EventSource.java
+++
@@ -1,9 +0,0 @@
-package csci318.demo.repository;
-
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-
-public interface EventSource {
-    @Output("orderPlacedChannel")
-    MessageChannel orderPlaced();
-}
