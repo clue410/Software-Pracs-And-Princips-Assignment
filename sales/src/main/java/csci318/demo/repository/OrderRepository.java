a47e2c3037245d8fc5d70019149435c7cc78f213
Youmna-Elshimy <98707244+Youmna-Elshimy@users.noreply.github.com>
2023-10-10 22:07:01 +1100

    Delete sales directory

--- sales/src/main/java/csci318/demo/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-
-package csci318.demo.repository;
-
-import csci318.demo.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
